73f21982f9c5e0eaf132c26b1f77152aa4da8643
sydong001 <sylidong001@gmail.com>
2022-06-02 15:06:03 +0900

    lidong

---
+++ Mall/src/main/java/com/example/demo/model/CmsPrefrenceAreaProductRelation.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+//优选专区和商品的关系
+public class CmsPrefrenceAreaProductRelation {
+	private Integer id;
+	private Integer prefrenceAreaId;
+	private Integer productId;
+}
