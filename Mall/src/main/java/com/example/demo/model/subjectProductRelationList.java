c821eda985799f923834567de8c8d45096f706d7
ZYooong <mx306639908@gmail.com>
2022-06-02 13:15:11 +0900

    1

---
+++ Mall/src/main/java/com/example/demo/model/subjectProductRelationList.java
@@ -0,0 +1,7 @@
+package com.example.demo.model;
+
+public class subjectProductRelationList {
+	private Integer id;//($int64)
+	private Integer productId;//($int64)
+	private Integer subjectId;//integer($int64)
+}
