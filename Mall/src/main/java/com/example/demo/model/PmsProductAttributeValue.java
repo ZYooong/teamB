36df8b736e5306447b1d6b8cbb4fbfc7b88ee0df
ZYooong <103733884+ZYooong@users.noreply.github.com>
2022-06-06 17:34:47 +0900

    Delete PmsProductAttributeValue.java

--- Mall/src/main/java/com/example/demo/model/PmsProductAttributeValue.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public class PmsProductAttributeValue {
-	private Long id;
-	private Long productAttributeId;
-	private Long productId;
-	private String value;
-} // 手动添加规格或参数的值,参数单值，规格有多个时以逗号隔开
