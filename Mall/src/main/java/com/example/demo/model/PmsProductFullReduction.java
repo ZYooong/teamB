45a4303126294191bf7a280704f86f91061477b3
ZYooong <103733884+ZYooong@users.noreply.github.com>
2022-06-06 17:32:48 +0900

    Delete PmsProductFullReduction.java

--- Mall/src/main/java/com/example/demo/model/PmsProductFullReduction.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public class PmsProductFullReduction {
-	private Double fullPrice;
-	private Long id;
-	private Long productId;
-	private Double reducePrice;
-	
-}
