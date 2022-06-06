e754718ad9f85b0003be03c5dca9c13d5c6369ef
sydong001 <sylidong001@gmail.com>
2022-06-06 18:05:34 +0900

    gg

---
+++ Mall/src/main/java/com/example/demo/repository/PmsProductLadderRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.PmsProductLadder;
+
+public interface PmsProductLadderRepository extends JpaRepository<PmsProductLadder, Long>{
+
+}
