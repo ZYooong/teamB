907d9591e2034b77b896fdbc4bda6b1b5728b1cb
ZYooong <mx306639908@gmail.com>
2022-06-06 17:08:57 +0900

    a

--- Mall/src/main/java/com/example/demo/model/CommonPage.java
+++ Mall/src/main/java/com/example/demo/model/CommonPage.java
@@ -2,9 +2,8 @@ package com.example.demo.model;
 
 import java.util.List;
 
-import lombok.Data;
-@Data
-public class CommonPagePmsProduct {
+
+public class CommonPage {
 	private Integer pageNum;//($int32)
 	private Integer pageSize;//($int32)
 	private Long total;//($int64)
