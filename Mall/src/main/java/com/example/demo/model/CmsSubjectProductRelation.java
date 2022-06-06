b02f5138f2b39c6465c42e5a059a3936f4158a2e
ZYooong <103733884+ZYooong@users.noreply.github.com>
2022-06-06 17:34:56 +0900

    Delete CmsSubjectProductRelation.java

--- Mall/src/main/java/com/example/demo/model/CmsSubjectProductRelation.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model;
-
-public class CmsSubjectProductRelation {
-	private Long id;//($int64)
-	private Long productId;//($int64)
-	private Long subjectId;//integer($int64)
-}
