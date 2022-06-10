cc92f72cacd03e26cb985421b80b745720ff9566
sydong001 <sylidong001@gmail.com>
2022-06-10 10:01:37 +0900

    代码第二次更新

--- Mall/src/main/java/com/example/demo/controller/PmsProductController.java
+++ Mall/src/main/java/com/example/demo/controller/PmsProductController.java
@@ -1,40 +1,49 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.example.demo.model.request.ProductParam;
+
+import com.example.demo.model.PmsProduct;
+import com.example.demo.model.request.PmsProductParam;
+import com.example.demo.model.request.SearchProductParam;
 import com.example.demo.model.responses.CommonResult;
 import com.example.demo.service.PmsProductParamService;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@RequestMapping("/product") 
+@RequestMapping("/product")
 @Controller
 public class PmsProductController {
 	// Logger 用于记录服务器日志
 	@Autowired
 	PmsProductParamService pmsProductParamService;
+
 	@ResponseBody // 返回值为 ResponseBody 的内容
-	@PostMapping("/create") 
-	public CommonResult create(@RequestBody ProductParam productParam) { // 传入参数为 RequestBody （在文档中标识为 body）
+	@PostMapping("/create")
+	public CommonResult create(@RequestBody PmsProductParam productParam) { // 传入参数为 RequestBody （在文档中标识为 body）
 		try {
 			pmsProductParamService.create(productParam);
-
 			return CommonResult.success(productParam);
-		}catch(Exception ex) {
-			return CommonResult.fail(401L,null,	"Unauthorized");
+		} catch (Exception ex) {
+			return CommonResult.fail(401L, null, "Unauthorized");
 		}
-		
+
+	}
+
+	@GetMapping("/list")
+	@ResponseBody
+	public CommonResult list(SearchProductParam searchProductParam) {
+		try {
+			List<PmsProduct> listProduct = pmsProductParamService.search(searchProductParam);
+			return CommonResult.success(listProduct);
+		} catch (Exception ex) {
+			return CommonResult.fail(401L, null, "Unauthorized");
+		}
 	}
 
 }
